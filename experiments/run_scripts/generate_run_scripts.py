c2fdb3891182f06dd61517c9e7b5c61f6384b3d8
Fabing Li <lifabing1349212501@gmail.com>
2023-11-27 07:47:34 +0000

    replace our own traces

--- experiments/run_scripts/generate_run_scripts.py
+++ experiments/run_scripts/generate_run_scripts.py
@@ -2,41 +2,74 @@
 # Usage: python3 generate_run_scripts.py > run_scripts.sh
 
 
-DATE = "2023_0511" # Used as the folder name under experiments/ to hold all log results. To avoid collision of repeated experiments, may change date or append _v1, _v2, etc.
+DATE = "2023_uniform" # Used as the folder name under experiments/ to hold all log results. To avoid collision of repeated experiments, may change date or append _v1, _v2, etc.
 REMARK = "Artifacts"
-REPEAT =10 # Number of repetitive experiments.
+REPEAT = 1 # Number of repetitive experiments.
 FILELIST = [
-    #: Main results in Fig. 7 and 9
-    "data/openb_pod_list_default",
-    #: Fig. 14 Various proportion of non-GPU tasks
-    "data/openb_pod_list_cpu050",
-    "data/openb_pod_list_cpu100",
-    "data/openb_pod_list_cpu200",
-    "data/openb_pod_list_cpu250",
-    #: Fig. 11 Various proportion of GPU-sharing tasks
-    "data/openb_pod_list_gpushare100",
-    "data/openb_pod_list_gpushare40",
-    "data/openb_pod_list_gpushare60",
-    "data/openb_pod_list_gpushare80",
-    #: Fig. 13 Various proportion of tasks with GPU-type constraints
-    "data/openb_pod_list_gpuspec10",
-    "data/openb_pod_list_gpuspec20",
-    "data/openb_pod_list_gpuspec25",
-    "data/openb_pod_list_gpuspec33",
-    #: Fig. 12 Various proportion of multi-GPU tasks
-    "data/openb_pod_list_multigpu20",
-    "data/openb_pod_list_multigpu30",
-    "data/openb_pod_list_multigpu40",
-    "data/openb_pod_list_multigpu50",
+    "data/fabing_uniform_050_1_1", "data/fabing_uniform_050_2_1", "data/fabing_uniform_050_2_2", "data/fabing_uniform_050_3_1", "data/fabing_uniform_050_3_2",
+    "data/fabing_uniform_050_3_3", "data/fabing_uniform_050_5_1", "data/fabing_uniform_050_5_2", "data/fabing_uniform_050_5_3", "data/fabing_uniform_050_5_4",
+    "data/fabing_uniform_050_5_5", "data/fabing_uniform_050_10_1", "data/fabing_uniform_050_10_2", "data/fabing_uniform_050_10_3", "data/fabing_uniform_050_10_4",
+    "data/fabing_uniform_050_10_5", "data/fabing_uniform_050_10_6", "data/fabing_uniform_050_10_7", "data/fabing_uniform_050_10_8", "data/fabing_uniform_050_10_9",
+    "data/fabing_uniform_050_10_10", "data/fabing_uniform_050_15_1", "data/fabing_uniform_050_15_2", "data/fabing_uniform_050_15_3", "data/fabing_uniform_050_15_4",
+    "data/fabing_uniform_050_15_5", "data/fabing_uniform_050_15_6", "data/fabing_uniform_050_15_7", "data/fabing_uniform_050_15_8", "data/fabing_uniform_050_15_9",
+    "data/fabing_uniform_050_15_10", "data/fabing_uniform_050_15_11", "data/fabing_uniform_050_15_12", "data/fabing_uniform_050_15_13", "data/fabing_uniform_050_15_14",
+    "data/fabing_uniform_050_15_15", "data/fabing_uniform_050_20_1", "data/fabing_uniform_050_20_2", "data/fabing_uniform_050_20_3", "data/fabing_uniform_050_20_4",
+    "data/fabing_uniform_050_20_5", "data/fabing_uniform_050_20_6", "data/fabing_uniform_050_20_7", "data/fabing_uniform_050_20_8", "data/fabing_uniform_050_20_9",
+    "data/fabing_uniform_050_20_10", "data/fabing_uniform_050_20_11", "data/fabing_uniform_050_20_12", "data/fabing_uniform_050_20_13", "data/fabing_uniform_050_20_14",
+    "data/fabing_uniform_050_20_15", 
+    "data/fabing_uniform_100_1_1", "data/fabing_uniform_100_2_1", "data/fabing_uniform_100_2_2", "data/fabing_uniform_100_3_1",
+    "data/fabing_uniform_100_3_2", "data/fabing_uniform_100_3_3", "data/fabing_uniform_100_5_1", "data/fabing_uniform_100_5_2", "data/fabing_uniform_100_5_3",
+    "data/fabing_uniform_100_5_4", "data/fabing_uniform_100_5_5", "data/fabing_uniform_100_10_1", "data/fabing_uniform_100_10_2", "data/fabing_uniform_100_10_3",
+    "data/fabing_uniform_100_10_4", "data/fabing_uniform_100_10_5", "data/fabing_uniform_100_10_6", "data/fabing_uniform_100_10_7", "data/fabing_uniform_100_10_8",
+    "data/fabing_uniform_100_10_9", "data/fabing_uniform_100_10_10", "data/fabing_uniform_100_15_1", "data/fabing_uniform_100_15_2", "data/fabing_uniform_100_15_3",
+    "data/fabing_uniform_100_15_4", "data/fabing_uniform_100_15_5", "data/fabing_uniform_100_15_6", "data/fabing_uniform_100_15_7", "data/fabing_uniform_100_15_8",
+    "data/fabing_uniform_100_15_9", "data/fabing_uniform_100_15_10", "data/fabing_uniform_100_15_11", "data/fabing_uniform_100_15_12", "data/fabing_uniform_100_15_13",
+    "data/fabing_uniform_100_15_14", "data/fabing_uniform_100_15_15", "data/fabing_uniform_100_20_1", "data/fabing_uniform_100_20_2", "data/fabing_uniform_100_20_3",
+    "data/fabing_uniform_100_20_4", "data/fabing_uniform_100_20_5", "data/fabing_uniform_100_20_6", "data/fabing_uniform_100_20_7", "data/fabing_uniform_100_20_8",
+    "data/fabing_uniform_100_20_9", "data/fabing_uniform_100_20_10", "data/fabing_uniform_100_20_11", "data/fabing_uniform_100_20_12", "data/fabing_uniform_100_20_13",
+    "data/fabing_uniform_100_20_14", "data/fabing_uniform_100_20_15", "data/fabing_uniform_100_20_16", "data/fabing_uniform_100_20_17", "data/fabing_uniform_100_20_18",
+    "data/fabing_uniform_100_20_19", "data/fabing_uniform_100_20_20", 
+    "data/fabing_uniform_200_1_1", "data/fabing_uniform_200_2_1", "data/fabing_uniform_200_2_2",
+    "data/fabing_uniform_200_3_1", "data/fabing_uniform_200_3_2", "data/fabing_uniform_200_3_3", "data/fabing_uniform_200_5_1", "data/fabing_uniform_200_5_2",
+    "data/fabing_uniform_200_5_3", "data/fabing_uniform_200_5_4", "data/fabing_uniform_200_5_5", "data/fabing_uniform_200_10_1", "data/fabing_uniform_200_10_2",
+    "data/fabing_uniform_200_10_3", "data/fabing_uniform_200_10_4", "data/fabing_uniform_200_10_5", "data/fabing_uniform_200_10_6", "data/fabing_uniform_200_10_7",
+    "data/fabing_uniform_200_10_8", "data/fabing_uniform_200_10_9", "data/fabing_uniform_200_10_10", "data/fabing_uniform_200_15_1", "data/fabing_uniform_200_15_2",
+    "data/fabing_uniform_200_15_3", "data/fabing_uniform_200_15_4", "data/fabing_uniform_200_15_5", "data/fabing_uniform_200_15_6", "data/fabing_uniform_200_15_7",
+    "data/fabing_uniform_200_15_8", "data/fabing_uniform_200_15_9", "data/fabing_uniform_200_15_10", "data/fabing_uniform_200_15_11", "data/fabing_uniform_200_15_12",
+    "data/fabing_uniform_200_15_13", "data/fabing_uniform_200_15_14", "data/fabing_uniform_200_15_15", "data/fabing_uniform_200_20_1", "data/fabing_uniform_200_20_2",
+    "data/fabing_uniform_200_20_3", "data/fabing_uniform_200_20_4", "data/fabing_uniform_200_20_5", "data/fabing_uniform_200_20_6", "data/fabing_uniform_200_20_7",
+    "data/fabing_uniform_200_20_8", "data/fabing_uniform_200_20_9", "data/fabing_uniform_200_20_10", "data/fabing_uniform_200_20_11", "data/fabing_uniform_200_20_12",
+    "data/fabing_uniform_200_20_13", "data/fabing_uniform_200_20_14", "data/fabing_uniform_200_20_15", "data/fabing_uniform_200_20_16", "data/fabing_uniform_200_20_17",
+    "data/fabing_uniform_200_20_18", "data/fabing_uniform_200_20_19", "data/fabing_uniform_200_20_20", 
+    "data/fabing_uniform_250_1_1", "data/fabing_uniform_250_2_1",
+    "data/fabing_uniform_250_2_2", "data/fabing_uniform_250_3_1", "data/fabing_uniform_250_3_2", "data/fabing_uniform_250_3_3", "data/fabing_uniform_250_5_1",
+    "data/fabing_uniform_250_5_2", "data/fabing_uniform_250_5_3", "data/fabing_uniform_250_5_4", "data/fabing_uniform_250_5_5", "data/fabing_uniform_250_10_1",
+    "data/fabing_uniform_250_10_2", "data/fabing_uniform_250_10_3", "data/fabing_uniform_250_10_4", "data/fabing_uniform_250_10_5", "data/fabing_uniform_250_10_6",
+    "data/fabing_uniform_250_10_7", "data/fabing_uniform_250_10_8", "data/fabing_uniform_250_10_9", "data/fabing_uniform_250_10_10", "data/fabing_uniform_250_15_1",
+    "data/fabing_uniform_250_15_2", "data/fabing_uniform_250_15_3", "data/fabing_uniform_250_15_4", "data/fabing_uniform_250_15_5", "data/fabing_uniform_250_15_6",
+    "data/fabing_uniform_250_15_7", "data/fabing_uniform_250_15_8", "data/fabing_uniform_250_15_9", "data/fabing_uniform_250_15_10", "data/fabing_uniform_250_15_11",
+    "data/fabing_uniform_250_15_12", "data/fabing_uniform_250_15_13", "data/fabing_uniform_250_15_14", "data/fabing_uniform_250_15_15", "data/fabing_uniform_250_20_1",
+    "data/fabing_uniform_250_20_2", "data/fabing_uniform_250_20_3", "data/fabing_uniform_250_20_4", "data/fabing_uniform_250_20_5", "data/fabing_uniform_250_20_6",
+    "data/fabing_uniform_250_20_7", "data/fabing_uniform_250_20_8", "data/fabing_uniform_250_20_9", "data/fabing_uniform_250_20_10", "data/fabing_uniform_250_20_11",
+    "data/fabing_uniform_250_20_12", "data/fabing_uniform_250_20_13", "data/fabing_uniform_250_20_14", "data/fabing_uniform_250_20_15", "data/fabing_uniform_250_20_16",
+    "data/fabing_uniform_250_20_17", "data/fabing_uniform_250_20_18", "data/fabing_uniform_250_20_19", "data/fabing_uniform_250_20_20", 
+    "data/fabing_uniform_300_1_1",
+    "data/fabing_uniform_300_2_1", "data/fabing_uniform_300_2_2", "data/fabing_uniform_300_3_1", "data/fabing_uniform_300_3_2", "data/fabing_uniform_300_3_3",
+    "data/fabing_uniform_300_5_1", "data/fabing_uniform_300_5_2", "data/fabing_uniform_300_5_3", "data/fabing_uniform_300_5_4", "data/fabing_uniform_300_5_5",
+    "data/fabing_uniform_300_10_1", "data/fabing_uniform_300_10_2", "data/fabing_uniform_300_10_3", "data/fabing_uniform_300_10_4", "data/fabing_uniform_300_10_5",
+    "data/fabing_uniform_300_10_6", "data/fabing_uniform_300_10_7", "data/fabing_uniform_300_10_8", "data/fabing_uniform_300_10_9", "data/fabing_uniform_300_10_10",
+    "data/fabing_uniform_300_15_1", "data/fabing_uniform_300_15_2", "data/fabing_uniform_300_15_3", "data/fabing_uniform_300_15_4", "data/fabing_uniform_300_15_5",
+    "data/fabing_uniform_300_15_6", "data/fabing_uniform_300_15_7", "data/fabing_uniform_300_15_8", "data/fabing_uniform_300_15_9", "data/fabing_uniform_300_15_10",
+    "data/fabing_uniform_300_15_11", "data/fabing_uniform_300_15_12", "data/fabing_uniform_300_15_13", "data/fabing_uniform_300_15_14", "data/fabing_uniform_300_15_15",
+    "data/fabing_uniform_300_20_1", "data/fabing_uniform_300_20_2", "data/fabing_uniform_300_20_3", "data/fabing_uniform_300_20_4", "data/fabing_uniform_300_20_5",
+    "data/fabing_uniform_300_20_6", "data/fabing_uniform_300_20_7", "data/fabing_uniform_300_20_8", "data/fabing_uniform_300_20_9", "data/fabing_uniform_300_20_10",
+    "data/fabing_uniform_300_20_11", "data/fabing_uniform_300_20_12", "data/fabing_uniform_300_20_13", "data/fabing_uniform_300_20_14", "data/fabing_uniform_300_20_15",
+    "data/fabing_uniform_300_20_16", "data/fabing_uniform_300_20_17", "data/fabing_uniform_300_20_18", "data/fabing_uniform_300_20_19", "data/fabing_uniform_300_20_20"
 ]
 
+
 AllMethodList = [
-    ["01", "Random", "random", "<none>", "<none>"],
-    ["02", "DotProd", "best", "merge", "max"],
-    ["03", "GpuClustering", "<none>", "<none>", "<none>"],
-    ["04", "GpuPacking", "<none>", "<none>", "<none>"],
-    ["05", "BestFit", "<none>", "<none>", "<none>"],
-    ["06", "FGD", "<self>", "share", "max"],
+    ["05", "BestFit", "<none>", "<none>", "<none>"]
 ]
 
 AllMethodDict = {}
@@ -50,12 +83,7 @@ for item in AllMethodList:
 MethodList = AllMethodList.copy()
 
 MethodList = [
-    ["01", "Random", "random", "<none>", "<none>"],
-    ["02", "DotProd", "best", "merge", "max"],
-    ["03", "GpuClustering", "<none>", "<none>", "<none>"],
-    ["04", "GpuPacking", "<none>", "<none>", "<none>"],
-    ["05", "BestFit", "<none>", "<none>", "<none>"],
-    ["06", "FGD", "<self>", "share", "max"],
+    ["05", "BestFit", "<none>", "<none>", "<none>"]
 ]
 
 def get_dir_name_from_method(method_input):
@@ -100,7 +128,7 @@ def generate_run_scripts(asyncc=True, parallel=16):
                     dir_name = get_dir_name_from_method([id, policy, gsm, dem, nm])
                     gsm = policy if gsm == "<self>" else gsm
                     OUTPUT_YAML = False
-                    SHUFFLE_POD = True
+                    SHUFFLE_POD = False
                     outstr = "# %s, %s, %s, %s, %s @ %s\n" % (id, policy, gsm, dem, nm, filename)
                     outstr += 'EXPDIR="experiments/%s/%s/%s/%s/%s' % (DATE, filename, dir_name, tune_ratio, tune_seed)
                     outstr += '" && mkdir -p ${EXPDIR} && touch "${EXPDIR}/terminal.out" && '
@@ -111,8 +139,8 @@ def generate_run_scripts(asyncc=True, parallel=16):
                     outstr += '-gpusel %s ' % gsm if gsm != "<none>" else ''
                     outstr += '-dimext %s ' % dem if dem != "<none>" else ''
                     outstr += '-norm %s ' % nm if nm != "<none>" else ''
-                    outstr += '-tune %s ' % tune_ratio if tune_ratio else ''
-                    outstr += '-tuneseed %s ' % tune_seed if tune_seed else ''
+                    outstr += '-tune %s ' % '0'
+                    outstr += '-tuneseed %s ' % '0'
                     outstr += "--shuffle-pod=true " if SHUFFLE_POD else ""
                     outstr += '-y "${EXPDIR}/snapshot/yaml" ' if OUTPUT_YAML else ""
                     outstr += '-z "${EXPDIR}/snapshot/ds01" '
